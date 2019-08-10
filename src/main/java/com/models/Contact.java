7ac158e72a0a6924c2f936b56053f9435dd6fccb
Marina Moreira <mariidepazzi@gmail.com>
2019-08-10 18:49:40 -0300

    Building Contact class (getters, setters, ...)

---
+++ src/main/java/com/models/Contact.java
@@ -0,0 +1,69 @@
+package com.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Contact {
+
+    @Id
+    @JsonIgnore
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Campo nome é obrigatório")
+    private String name;
+
+    @NotBlank(message = "Campo email é obrigatório")
+    private String email;
+
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Contact(Long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
