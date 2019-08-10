1a6f95a53feb188f68cb6c4c7f971d689d834737
Marina Moreira <mariidepazzi@gmail.com>
2019-08-10 19:00:47 -0300

    Contact services added

---
+++ src/main/java/com/apirestproject/marinadepazzi/firstapirest/services/ContactServices.java
@@ -0,0 +1,36 @@
+package com.apirestproject.marinadepazzi.firstapirest.services;
+
+import com.apirestproject.marinadepazzi.firstapirest.models.Contact;
+import com.apirestproject.marinadepazzi.firstapirest.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContactServices {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public Optional<Contact> findById(Long id){
+        return contactRepository.findById(id);
+    }
+
+    public List<Contact> findAll(){
+        return contactRepository.findAll();
+    }
+
+    public Contact save(Contact contact){
+        return contactRepository.save(contact);
+    }
+
+    public void deleteById(long id){
+        contactRepository.deleteById(id);
+    }
+
+    public void deleteAll(){
+        contactRepository.deleteAll();
+    }
+}
