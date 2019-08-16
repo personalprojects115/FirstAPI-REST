fce0769c1692fc217757192c848468368115d753
Marina Moreira <mariidepazzi@gmail.com>
2019-08-16 14:14:30 -0300

    1º part done

--- src/main/java/com/apirestproject/marinadepazzi/firstapirest/controllers/ContactController.java
+++ src/main/java/com/apirestproject/marinadepazzi/firstapirest/controllers/ContactController.java
@@ -1,17 +1,24 @@
 package com.apirestproject.marinadepazzi.firstapirest.controllers;
 
-import com.apirestproject.marinadepazzi.firstapirest.models.Contact;
-import com.apirestproject.marinadepazzi.firstapirest.services.ContactService;
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
+import com.apirestproject.marinadepazzi.firstapirest.models.Contact;
+import com.apirestproject.marinadepazzi.firstapirest.services.ContactService;
 
 @RestController
 @RequestMapping("/contacts")
@@ -20,11 +27,44 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    @GetMapping
+    public List<Contact> findAll(){
+        return contactService.findAll();
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Contact> findById(@PathVariable Long id){
+        return contactService.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public ResponseEntity<Contact> create(@Valid @RequestBody Contact contact){
         Contact obj = contactService.save(contact);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity update(@PathVariable("id") long id,
+                                 @RequestBody Contact contact) {
+        return contactService.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updated = contactService.save(record);
+                    return ResponseEntity.accepted().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable long id) {
+        return contactService.findById(id)
+                .map(record -> {
+                    contactService.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
